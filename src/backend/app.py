d5a803dda9715d370aea031be3695bfd2ef1f527
SantiagoTaracena <staracenapuga@gmail.com>
2024-03-27 18:14:30 -0600

    SignUp functional and finished

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,9 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 from dotenv import load_dotenv
 import os
 import psycopg2
+import datetime
 
 app = Flask(__name__)
+CORS(app)
 
 load_dotenv()
 
@@ -12,7 +15,6 @@ postgres_password = os.getenv("POSTGRES_PASSWORD")
 postgres_host = os.getenv("POSTGRES_HOST")
 postgres_db = os.getenv("POSTGRES_DB")
 
-# ! Cambiar el .env para usar las variables que son.
 conn = psycopg2.connect(
     dbname=postgres_db,
     user=postgres_user,
@@ -27,10 +29,41 @@ def hello():
 @app.get("/users")
 def get_users():
     cur = conn.cursor()
-    cur.execute("SELECT * FROM users")
+    cur.execute("SELECT * FROM Usuario")
     rows = cur.fetchall()
     cur.close()
     return jsonify(rows)
+
+
+@app.get("/users/<string:user>/key")
+def get_user_key(user):
+    cur = conn.cursor()
+    cur.execute(f"SELECT public_key FROM Usuario WHERE username = '{user}'")
+    rows = cur.fetchall()
+    cur.close()
+    return jsonify(rows)
+
+@app.get("/messages/<string:user>")
+def get_user_messages(user):
+    cur = conn.cursor()
+    cur.execute(f"SELECT * FROM Mensajes WHERE username_destino = '{user}'")
+    rows = cur.fetchall()
+    cur.close()
+    return jsonify(rows)
+
+@app.post("/users")
+def post_user():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+    cur = conn.cursor()
+    cur.execute("SELECT MAX(id) FROM Usuario")
+    rows = cur.fetchall()
+    max_id = rows[0][0]
+    cur.execute(f"INSERT INTO Usuario (id, public_key, username, fecha_creacion, password) VALUES ({max_id + 1}, 'test-public-key', '{username}', '{datetime.date.today()}', '{password}')")
+    conn.commit()
+    cur.close()
+    return "200"
 
 if (__name__ == "__main__"):
     app.run(debug=True)
--- src/frontend/src/pages/SignUp.jsx
+++ src/frontend/src/pages/SignUp.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
 import Button from '../components/Button'
 import '../styles/sign-up.sass'
 
 const SignUp = () => {
   const [formData, setFormData] = useState({
     username: '',
-    email: '',
     password: ''
   })
 
@@ -15,9 +15,14 @@ const SignUp = () => {
     setFormData({ ...formData, [name]: value })
   }
 
+  const navigate = useNavigate()
+
   const handleSubmit = (event) => {
     event.preventDefault()
-    console.log(formData)
+    axios.post(`${import.meta.env.VITE_APP_API_URL}/users`, formData)
+    .then((response) => alert(`Usuario creado con status ${response}`))
+    .catch((error) => console.error('Error al realizar la solicitud', error))
+    navigate('/')
   }
 
   return (
@@ -40,17 +45,6 @@ const SignUp = () => {
             />
           </div>
           <div className="sign-up-input-entry">
-            <label htmlFor="email">Correo electrónico:</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div className="sign-up-input-entry">
             <label htmlFor="password">Contraseña:</label>
             <input
               type="password"
@@ -62,7 +56,7 @@ const SignUp = () => {
             />
           </div>
           <div className="sign-up-buttons">
-            <Link to="/"><Button buttonText="Registrarse" type="submit" /></Link>
+            <Button buttonText="Registrarse" type="submit" onClick={handleSubmit} />
             <Link to="/"><Button buttonText="Volver" type="button" /></Link>
           </div>
         </form>

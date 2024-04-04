9e4dbe147e89ffad40f0ac5b12aaf5224dae3523
SantiagoTaracena <staracenapuga@gmail.com>
2024-04-04 16:21:20 -0600

    Writing user info

--- src/backend/app.py
+++ src/backend/app.py
@@ -139,8 +139,8 @@ def post_user():
     """)
     conn.commit()
     cur.close()
-    with open("private-key.txt", mode="w") as file:
-        file.write(private_key)
+    with open("users.txt", mode="a") as file:
+        file.write(private_key + "\n")
     return jsonify({ "status": 200, "private_key": private_key })
 
 @app.post("/users/<string:user>")
